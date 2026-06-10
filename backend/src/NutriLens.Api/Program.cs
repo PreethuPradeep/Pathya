13c9015964f0ad600ac81646a29e9eb9399b2f32
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2026-06-10 23:22:32 +0530

    feat: add health endpoint

--- backend/src/NutriLens.Api/Program.cs
+++ backend/src/NutriLens.Api/Program.cs
@@ -14,28 +14,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/health", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+    return Results.Ok(new
+    {
+        Status = "Healthy",
+        Application = "NutriLens",
+        TimeStamp = DateTime.UtcNow
+    });
+});
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
